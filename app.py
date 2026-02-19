c69706bc5b8efe4826c399e54826e808d2730b5e
Jackdevelops <jackcampbell582@gmail.com>
2026-02-19 16:23:46 +0000

    test

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     x = 5
-    return  'Hello, World! <br><br> Welcome to Version 1 Digital Skills Academy <br><br> Lets create a variable: <br><br> x =' + x
+    return  'Hello, World! <br><br> Welcome to Version 1 Digital Skills Academy <br><br> Lets create a variable: <br><br> x =' 
             
